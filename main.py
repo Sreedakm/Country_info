fdacd1581b8b8c6c191a5b7f5406adaaae6b32ba
Sreedakm <sreedakm@gmail.com>
2026-06-02 18:21:47 +0530

    modified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-country= input("Enter a country: ")
-
 import requests
+import wikipediaapi
+
+country= input("Enter a country: ")
 
 url= f"https://restcountries.com/v3.1/name/{country}"
 response= requests.get(url)
@@ -12,3 +13,8 @@ print(f"\nPopulation: {country_data['population']:,}")
 print(f"\nArea: {country_data['area']:,} sq km")
 print(f"\nLanguages: {','.join(country_data['languages'].values())}")
 print(f"\nTimezones: {country_data['timezones'][0]}")
+
+wiki = wikipediaapi.Wikipedia('country-info-tool','en')
+page = wiki.page(country_data['name']['common'])
+if page.exists():
+    print(f"\nSummary: {page.summary[:300]}\n")
