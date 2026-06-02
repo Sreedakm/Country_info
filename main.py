3d1e9af8aca736db8d6a5cdbf88a6957f2f569f4
Sreedakm <sreedakm@gmail.com>
2026-06-02 18:53:08 +0530

    Made changes

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import wikipediaapi
 
 country= input("Enter a country: ")
 
+
 url= f"https://restcountries.com/v3.1/name/{country}"
 response= requests.get(url)
+if response.status_code != 200:
+    print('Country not found. Please check the spelling and try again.')
+    exit()
 data= response.json()
 country_data= data[0]
 print(f"\nCountry: {country_data['name']['common']}")
@@ -17,4 +21,7 @@ print(f"\nTimezones: {country_data['timezones'][0]}")
 wiki = wikipediaapi.Wikipedia('country-info-tool','en')
 page = wiki.page(country_data['name']['common'])
 if page.exists():
-    print(f"\nSummary: {page.summary[:300]}\n")
+    sentences = page.summary.split('.')
+    short_summary = '. '.join(sentences[:3]) + '.'
+    print(f"\nSummary: {short_summary}\n")
+    
